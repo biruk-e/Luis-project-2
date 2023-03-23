7cefbe677f6fdb63c03950c6a53b236cfd13ada1
biruk-e <113292687+biruk-e@users.noreply.github.com>
2023-03-23 00:52:00 -0400

    Update luis_proj_streamlit.py

--- luis_proj_streamlit.py
+++ luis_proj_streamlit.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Luis Project Temp. Website")
+st.title("Project Title")
 st.header("Program Description")
 st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
 st.header("Instructions")
